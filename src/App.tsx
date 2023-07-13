8eae007a896cc57ed0b9b2e7db9d4c03b723c801
Andualem-Andy <andualemasamenew5@.com>
2023-07-13 21:12:01 +0300

    Build Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import { Button } from "@chakra-ui/react";
 import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav""aside main"`, // large device wider than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="Aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
